c31760485864894d4e18b0a8c707056204392aec
dhanraj-saur <121377364+dhanraj-saur@users.noreply.github.com>
2023-03-17 00:37:00 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
 //your JS code here. If required.
+function promise() {
+    return new Promise((resolve, reject) => {
+      setTimeout(() => {
+        resolve("Hello, world!");
+      }, 1000);
+    });
+	
+  }
+document.getElementById("output").value
